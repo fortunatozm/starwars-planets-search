0777408451dc7784dde6b00fc6175352df9c5167
fortunatozm <fortunatompongo@hotmail.com>
2022-05-06 00:49:53 -0300

    1 - Faça uma requisição para o endpoint /planets da API de Star Wars e preencha uma tabela com os dados retornados, com exceção dos da coluna residents - Corrigido (API)

--- src/apiService/apiPlanets.js
+++ src/apiService/apiPlanets.js
@@ -1,10 +1,10 @@
 import apiResults from './apiResults.json';
 
-const funcAPI = async () => apiResults;
-// {
-//   const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
-//   const responseJSON = await response.json();
-//   return responseJSON;
-// };
+const funcAPI = async () => {
+    // apiResults;
+  const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+  const responseJSON = await response.json();
+  return responseJSON;
+};
 
 export default funcAPI;
